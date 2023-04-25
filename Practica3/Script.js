caa57ce4a36196b55a49c8f44e3eae66415ee83f
Albaesc <118297753+Albaesc@users.noreply.github.com>
2023-04-25 17:51:06 +0200

    Create Script.js

---
+++ Practica3/Script.js
@@ -0,0 +1,5 @@
+if(window.file && window.FileReader && window.FileList){
+
+  const videoUploaded=document.getElementById("video-uploaded");
+  const playButton=document.getElementById("play-button");
+  const
